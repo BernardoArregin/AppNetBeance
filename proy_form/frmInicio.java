7547b4770132776c6400a0fa33f0fc3e9c6a9647
BernardoArregin <arreginbernardo@gmail.com>
2024-08-29 16:21:50 -0300

    Add files via upload

---
+++ proy_form/frmInicio.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proy_form;
+
+/**
+ *
+ * @author 57081384
+ */
+public class frmInicio {
+
+    void setVisible(boolean b) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
